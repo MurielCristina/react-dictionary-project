dec620ba7f4449985c010242b775647f1112637b
Muri <mcsanchezlaguna@gmail.com>
2024-09-05 15:12:53 +0200

    added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Designer from "./Designer.png";
 
+import Dictionary from "./Dictionary";
 import Footer from "./Footer";
 
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Designer} className="App-logo" alt="logo" />
-        <span className="App-text">Muri</span>
-      </header>
-      <div className="btn btn-primary">xxxx</div>
-      <Footer />
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={Designer} className="App-logo" alt="logo" />
+          <span className="App-text">Muri</span>
+        </header>
+        <Dictionary />
+        <Footer />
+      </div>
     </div>
   );
 }
