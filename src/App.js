f212885d34d4abe55ce02c149c97e9e95bd402e0
Muri <mcsanchezlaguna@gmail.com>
2024-09-05 14:34:01 +0200

    Remove unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Designer from "./Designer.png";
 
-function App() {
+import Footer from "./Footer";
+
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Designer} className="App-logo" alt="logo" />
+        <span className="App-text">Muri</span>
       </header>
     </div>
   );
 }
-
-export default App;
