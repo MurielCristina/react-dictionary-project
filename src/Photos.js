097f0723d935a30b3edb84eaab701f218a274545
Muri <mcsanchezlaguna@gmail.com>
2024-09-12 13:28:32 +0200

    integration fot the photo section

--- src/Photos.js
+++ src/Photos.js
@@ -2,7 +2,25 @@ import React from "react";
 
 export default function Photos(props) {
   if (props.photos) {
-    return <div className="Photos">xxxxx</div>;
+    return (
+      <div className="Photos">
+        <div className="row">
+          {props.photos.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.original}
+                    alt={photo.alt}
+                    className="img-fluid rounded mb-2 d-block"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   } else {
     return null;
   }
